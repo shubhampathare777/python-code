13ee5c5ed4703d8f72dc9204111c2bc5cf6b3384
Shubham Jagannath Pathare <shubhampathare044@gmail.com>
2024-02-09 11:40:22 +0530

    change

--- day_2/string.py
+++ day_2/string.py
@@ -1,4 +1,4 @@
-print("this is 'shubham'pathare")
+'''print("this is 'shubham'pathare")
 a="shubham"
 b="pathare"
 c=a+" "+b
@@ -12,3 +12,21 @@ str=str.capitalize()
 print(str)
 str=str.replace("h","b")
 print(str)
+d=str.find("q")
+if d==0:
+   print("strat") 
+else: 
+    print("invalid")'''
+
+'''str="maval shubham pathare vadgaon maval"
+a=str.count("a")
+b=str.count("e") 
+c=str.count("i") 
+d=str.count("o")
+f=str.count("u") 
+print(a,b,c,d,f)
+v=str.replace(("a")("e"),"x")
+print(v)'''
+
+e="25$ $ $ sguahkjdn $"
+print(e.count("$"))
