0416ad964d34a59ad28e6abe80ccc6870a45b540
Shubham Jagannath Pathare <shubhampathare044@gmail.com>
2024-02-09 10:46:08 +0530

    code

---
+++ day_2/string.py
@@ -0,0 +1,14 @@
+print("this is 'shubham'pathare")
+a="shubham"
+b="pathare"
+c=a+" "+b
+print(c)
+print(len(c))
+q=c[0:]
+print(q)
+str="shubham"
+print(str.endswith("am"))
+str=str.capitalize()
+print(str)
+str=str.replace("h","b")
+print(str)
