04eeff3304c12ef95b6daa52b0c33203ce2cff66
Shubham Jagannath Pathare <shubhampathare044@gmail.com>
2024-02-12 20:12:34 +0530

    new

---
+++ day_4/loops.py
@@ -0,0 +1,6 @@
+for i in range (5):
+    print("hellow")
+count=1    
+while count <= 3:
+    print("shubham")
+    count+=1    
---
+++ day_4/while.py
@@ -0,0 +1,29 @@
+'''i = 1
+while i <= 100:
+    print(i)
+    i += 1
+print("loop ended")    
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+print("loop ended")  
+
+#multiplication of number n
+i=2
+while i<=20:
+    print(i)
+    i=i+2
+
+
+n=int(input("enter the number to multyply:"))
+i=1
+while i <= 10:
+    print(n*i)
+    i += 1'''
+    
+for i in range(11):
+    print(i**2)
+
+    
