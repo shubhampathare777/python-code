76e09e80b1eb184d427a0b7bf1ca6f3c573cd448
Shubham Jagannath Pathare <shubhampathare044@gmail.com>
2024-02-14 12:19:55 +0530

    new

--- day_4/test2.py
+++ day_4/test2.py
@@ -1,5 +1,8 @@
 # for i in range (11):
 #     print(i**2)
 #     i+=1
-for i in range(3,31,3):
-    print(i)
+# for i in range(3,31,3):
+#     print(i)
+    
+for j in range (1,101,2):
+    print(j)
